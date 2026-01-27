2c71604d08ff3a53b28f9896586ede2033553590
Nahom <nahiyo86@gmail.com>
2026-01-27 15:53:16 +0300

    chore: improve lighting, camera, backround of image

--- blender/step1_still.py
+++ blender/step1_still.py
@@ -8,15 +8,38 @@ bpy.ops.object.delete()
 
 # Add cube
 bpy.ops.mesh.primitive_cube_add(size=2, location=(0, 0, 0))
+cube = bpy.context.object
+
+# Material (IMPORTANT)
+mat = bpy.data.materials.new(name="CubeMaterial")
+mat.use_nodes = True
+bsdf = mat.node_tree.nodes["Principled BSDF"]
+bsdf.inputs["Base Color"].default_value = (0.2, 0.6, 1.0, 1)
+bsdf.inputs["Roughness"].default_value = 0.4
+cube.data.materials.append(mat)
 
 # Camera
-bpy.ops.object.camera_add(location=(6, -6, 4))
+bpy.ops.object.camera_add(location=(5, -5, 4))
 camera = bpy.context.object
 camera.rotation_euler = (1.1, 0, 0.8)
 bpy.context.scene.camera = camera
 
-# Light
-bpy.ops.object.light_add(type='AREA', location=(4, -4, 6))
+# Key Light
+bpy.ops.object.light_add(type='AREA', location=(4, -4, 5))
+key = bpy.context.object
+key.data.energy = 800
+
+# Fill Light
+bpy.ops.object.light_add(type='POINT', location=(-4, -2, 3))
+fill = bpy.context.object
+fill.data.energy = 300
+
+# World background (contrast!)
+world = bpy.context.scene.world
+world.use_nodes = True
+bg = world.node_tree.nodes["Background"]
+bg.inputs[0].default_value = (1, 1, 1, 1)
+bg.inputs[1].default_value = 0.8
 
 # Render settings
 scene = bpy.context.scene
